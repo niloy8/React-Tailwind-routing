c0f96ef695ba3751950312b04c7aaba1223eace0
niloy8 <niloybhuiyan16810@gmail.com>
2024-02-21 11:25:30 +0600

    First commit

---
+++ src/components/NAV/Navbar.jsx
@@ -0,0 +1,17 @@
+const routes = [
+    { id: 1, name: 'Home', path: '/' },
+    { id: 2, name: 'About', path: '/about' },
+    { id: 3, name: 'Services', path: '/services' },
+    { id: 4, name: 'Products', path: '/products' },
+    { id: 5, name: 'Contact', path: '/contact' }
+];
+
+const Navbar = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Navbar;
